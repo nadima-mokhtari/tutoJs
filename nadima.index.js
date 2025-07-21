eb2614de4a0ea5e5c0aaa8c4338892d2ef48c765
nadima-mokhtari <nadima.mokhtari.dwwm48@gmail.com>
2025-07-21 16:02:09 +0200

    update

--- nadima.index.js
+++ nadima.index.js
@@ -1,42 +1,26 @@
 const toggleButton = document.getElementById('toggleButton');
-  toggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark'); // Toggle dark mode
-  });
-
-
-<<<<<<< HEAD
-
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode'); 
+});
 
 document.getElementById('formContact').addEventListener('submit', function(event) {
-    
-    event.preventDefault();
-  
-    const formName = document.getElementById('nom').value;
-    const formNumber = document.getElementById('personne').value;
-    let valid = true;
+  event.preventDefault();
 
-=======
-document.getElementById('formContact').addEventListener('submit', function(event) {
-    
-    event.preventDefault();
-  
-    const formName = document.getElementById('nom').value;
-    const formNumber = document.getElementById('personne').value;
-    let valid = true;
+  const formName = document.getElementById('nom').value;
+  const formNumber = document.getElementById('personne').value;
+  let valid = true;
 
->>>>>>> 63e7aafa700f22681fccbb55eb0bff067501de61
-    if (formName.length < 3) {
-        alert('Le nom doit contenir au moins 3 caractères.');
-        valid = false;
-    }
+  if (formName.length < 3) {
+    alert('Le nom doit contenir au moins 3 caractères.');
+    valid = false;
+  }
 
-    if (!Number.isInteger(Number(formNumber)) || Number(formNumber) < 1) {
-        alert('Le nombre de personnes doit être un entier.');
-        valid = false;
-    }
+  if (!Number.isInteger(Number(formNumber)) || Number(formNumber) < 1) {
+    alert('Le nombre de personnes doit être un entier.');
+    valid = false;
+  }
 
-    
-    if (valid) {
-        this.submit();
-    }
+  if (valid) {
+    this.submit();
+  }
 });
